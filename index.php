851ac16898a027666dafbb39a04555d8bf756888
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2011-11-10 04:55:20 -0200

    created gitignore, renamed .htaccess

--- index.php
+++ index.php
@@ -3,7 +3,12 @@ header('Location: mobi/');
 exit;
 ?>
 <!doctype html>
-<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]--><!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]--><!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]--><!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]--><!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]--><head>
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
@@ -17,7 +22,8 @@ exit;
     <link rel="apple-touch-icon" href="/apple-touch-icon.png">
     <link rel="stylesheet" href="css/style.css?v=2">
     <link rel="stylesheet" media="handheld" href="css/handheld.css?v=2">
-    <script src="js/libs/modernizr-1.7.min.js"></script></head>
+    <script src="js/libs/modernizr-1.7.min.js"></script>
+</head>
 <body>
     <div id="container">
         <header>
@@ -46,5 +52,6 @@ exit;
         (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
         g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
         s.parentNode.insertBefore(g,s)}(document,'script'));
-    </script></body>
+    </script>
+</body>
 </html>
